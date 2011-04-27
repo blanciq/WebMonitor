f946954a4940a4c32e6e0dbab0e4ad50cd74a71d
Paweł Olesiejuk <blanciq@o2.pl>
2011-04-28 00:12:43 +0200

    Simple project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebMonitor::Application.config.secret_token = 'a6a928c0561c66b7765fb9c07a76299d96295dd104cc8dd4d0a9ab87cca659198d6d1bbd8cee60091dbc9756a208b57a3fa51113b51860edfe4a8e6b714fc8fe'
