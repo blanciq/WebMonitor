0cb2765fe2f2a7441c80201dc223ab17123372c3
Paweł Olesiejuk <blanciq@o2.pl>
2011-07-30 15:11:08 +0200

    #2093 generated model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @sites = Site.all
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110716110219) do
+ActiveRecord::Schema.define(:version => 20110730130026) do
+
+  create_table "sites", :force => true do |t|
+    t.string   "name"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
