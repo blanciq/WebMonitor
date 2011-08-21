5494080d20e803e2f9dce38fd562514ad870a4b0
whr <whr@dev.wh>
2011-08-21 15:27:01 +0200

    Visual fixes

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -2,8 +2,10 @@ module WelcomeHelper
   def validation_image status
     if status == :ok
       file = "ok.png"     
-    else
+    elsif status == :error
       file = "error.png"
+    else
+      file = "question_mark.png"              
     end
     raw "<img src=\"/images/" + file + "\" alt=\"status\" class=\"validation-image\"/>"
   end
